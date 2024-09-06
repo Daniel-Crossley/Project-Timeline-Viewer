bd8240bde91627e66ea2881300a32c98ebce4e05
TurnTheKeys <tommy.hua.tran@gmail.com>
2024-09-06 16:22:28 +1000

    Timeline View - Added fields and methods
    Added fields and methods to timeline view using mapping created by Daniel

--- src/main/java/com/example/project/Timeline/TimelineView.java
+++ src/main/java/com/example/project/Timeline/TimelineView.java
@@ -12,22 +12,88 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TimelineView {
     @FXML
     private Label projectTitle;
-    private int projectID = 0;
-    private String projectColour = "#9FA1AC";
-    private cardEntry[] listOfCards;
+    private int timelineID = 0;
 
-    private LocalDateTime projectTimeStart;
-    private LocalDateTime projectTimeEnd;
-    private String projectDescription = "Project Test Description";
+
+    private List<cardEntry> listOfCards = new ArrayList<cardEntry>();
+
+    private String timelineTitleName = "Project Default Title";
+    private String timelineDescription = "Project Default Description";
+    private String timelineTimeCreated = "DD/MM/YYYY";
+    private String timelineTimeFinished = "DD/MM/YYYY";
+    private boolean timelineVisibility = true;
+    private int timelineLikes = 0;
+    private String timelineColour = "#9FA1AC";
+
 
 
     private GridPane cardSpace;
 
+    public String getTitle(){
+        return timelineTitleName;
+    }
+
+    private void setTitle(String timelineTitle){
+        this.timelineTitleName = timelineTitle;
+    }
+
+    public String getDescription (){
+        return timelineDescription;
+    }
+
+    private void setDescription(String timelineDescription){
+        this.timelineDescription = timelineDescription;
+    }
+
+    public String getDateCreated(){
+        return timelineTimeCreated;
+    }
+
+    private void setDateCreated(String timelineCreatedDate{
+        this.timelineTimeCreated = timelineCreatedDate;
+    }
+
+    public String getDateFinished(){
+        return timelineTimeFinished;
+    }
+
+    private void setDateFinished(String timelineFinishedDate){
+        this.timelineTimeFinished = timelineFinishedDate;
+    }
+
+    public boolean isVisible(){
+        return timelineVisibility;
+    }
+
+    public void setTimelineVisibility(boolean visibility){
+        this.timelineVisibility = visibility;
+    }
+
+    public int getTimelineLikes(){
+        return this.timelineLikes;
+    }
+
+    public void setTimelineLikes(int likes){
+        this.timelineLikes = likes;
+    }
+
+    public String getColour(){
+        return timelineColour;
+    }
+
+    public void setColour(String colour){
+        this.timelineColour = colour;
+    }
+
+    public void addCard(cardEntry cardAdd){
+        listOfCards.add(cardAdd);
+    }
 
     @FXML
     private void onReturnClick(ActionEvent actionEvent) {
@@ -60,9 +126,5 @@ public class TimelineView {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private boolean addCards(){
-        return false;
     }
 }
