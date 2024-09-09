3cafbc9ef6d751654e6971ea305409f0742b6edd
Jack Johnston <jackjj2004@hotmail.com>
2024-09-09 19:19:22 +1000

    Register system setup to mock database with no checks

--- src/main/java/com/example/project/RegisterController.java
+++ src/main/java/com/example/project/RegisterController.java
@@ -1,4 +1,51 @@
 package com.example.project;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.List;
+
 public class RegisterController {
+    @FXML
+    private ListView<User> contactsListView;
+    private IUserDAO userDAO;
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private Button onAdd;
+
+    @FXML
+    private void onAdd() {
+        // Default values for a new contact
+        String USERNAME = usernameField.getText();
+        String EMAIL = emailField.getText();
+        String PASSWORD = passwordField.getText();
+        String CONFIRM_PASSWORD = confirmPasswordField.getText();
+
+
+        User newUser = new User(USERNAME, EMAIL, PASSWORD);
+        // Add the new contact to the database
+        userDAO.addUser(newUser);
+
+        List<User> contacts = userDAO.getAllContacts();
+        for (User contact : contacts) {
+            System.out.println(contact.getUser());
+        }
+    }
+
+    public RegisterController() {
+        userDAO = new MockUserDAO();
+    }
 }
---
+++ src/main/java/com/example/project/User.java
@@ -0,0 +1,41 @@
+package com.example.project;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    //username
+
+    public String getUser() {
+        return username;
+    }
+
+    public void setUser(String username) {
+        this.username = username;
+    }
+
+    //email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
