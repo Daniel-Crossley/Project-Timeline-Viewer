559baa80ec8aaca2dd79680fd372606b24f31f15
Daniel-Crossley <daniel-crossley@aapt.net.au>
2024-09-14 15:04:55 +1000

    Developing projectsDAO

--- src/main/java/com/example/project/model/SqliteProjectDAO.java
+++ src/main/java/com/example/project/model/SqliteProjectDAO.java
@@ -1,4 +1,28 @@
 package com.example.project.model;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class SqliteProjectDAO {
+    private Connection connection;
+
+    public SqliteProjectDAO(){
+        connection = SqliteConnection.getInstance();
+        createTable();
+    }
+
+    private void createTable(){
+        try {
+            Statement statement = connection.createStatement();
+            String query = "CREATE TABLE IF NOT EXISTS Projects ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "title VARCHAR,"
+                    + "description VARCHAR,"
+                    + "dateCreated VARCHAR"
+                    + ")";
+            statement.execute(query);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
