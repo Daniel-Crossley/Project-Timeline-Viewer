a53682be08f003507b4cf2c957f0ff2ab9e0a680
TurnTheKeys <tommy.hua.tran@gmail.com>
2024-09-16 15:24:32 +1000

    Dashboard - Added Logout
    Added logout function

--- src/main/java/com/example/project/controller/DashboardController.java
+++ src/main/java/com/example/project/controller/DashboardController.java
@@ -1,14 +1,29 @@
 package com.example.project.controller;
 
+import com.example.project.ApplicationStart;
 import com.example.project.model.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
+    public Pane Menu;
+    public Button Button_Logout;
+    public Button Button_Search;
+    public Button Button_Dashboard;
+    public Pane Container_In_Progress;
+    public Pane Container_Completed;
     @FXML
     private Label Label_Username;
 
@@ -32,5 +47,17 @@ public class DashboardController implements Initializable {
         } else if (userInformation != null && userInformation.getUsername() != null) {
             Label_Username.setText("Welcome, " + userInformation.getUsername());
         }
+    }
+
+
+    public void Logout(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) Button_Logout.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationStart.class.getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, ApplicationStart.WIDTH, ApplicationStart.HEIGHT);
+        stage.setScene(scene);
+    }
+
+    public void Open_Search(ActionEvent actionEvent) {
     }
 }
